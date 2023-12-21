ae19d2a8e85adceb5334a598e134b9132608bcf6
swathimeenal <swathimeenal1994@gmail.com>
2023-12-21 07:05:07 +0530

    shopping-cart

---
+++ src/components/Card.jsx
@@ -0,0 +1,41 @@
+import React,{useState} from 'react'
+
+function Card({cart,setCart}) {
+    
+return <>
+<div className="col mb-5">
+  <div className="card h-100">
+  {/*<!-- Sale badge-->*/}
+    <div className="badge bg-dark text-white position-absolute" style={{top:"0.5rem", right:"0.5rem"}}>Sale</div>
+    {/*<!-- Product image-->*/}
+                            <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                           {/* <!-- Product details-->*/}
+                            <div className="card-body p-4">
+                                <div className="text-center">
+                                   {/* <!-- Product name-->*/}
+                                    <h5 className="fw-bolder">Special Item</h5>
+                                   {/* <!-- Product reviews-->*/}
+                                    <div className="d-flex justify-content-center small text-warning mb-2">
+                                        <div className="bi-star-fill"></div>
+                                        <div className="bi-star-fill"></div>
+                                        <div className="bi-star-fill"></div>
+                                        <div className="bi-star-fill"></div>
+                                        <div className="bi-star-fill"></div>
+                                    </div>
+                                   {/* <!-- Product price-->*/}
+                                    <span className="text-muted text-decoration-line-through">$20.00</span>
+                                    $18.00
+                                </div>
+                            </div>
+    {/*<!-- Product actions-->*/}
+    <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+    <div className="text-center"><button className="btn btn-outline-dark mt-auto" onClick={()=>{setCart(cart+1)}}>Add to Cart</button>
+    </div>
+    </div>
+ </div>
+</div>
+                   
+  </>
+}
+
+export default Card
