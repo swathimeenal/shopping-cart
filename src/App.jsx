380a38c2032d2009a6d2391d010f237f8336cac8
swathimeenal <swathimeenal1994@gmail.com>
2023-12-24 00:35:50 +0530

    shopping-cart1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React,{useState} from 'react'
 import Topbar from './components/Topbar'
-//import Header from './components/Header'
+import Header from './components/Header'
 import Card from './components/Card'
 
 function App() {
   let [cart,setCart] = useState(0)
   return <>
   <Topbar cart={cart} setCart={setCart}/>
+  <Header/>
   <section className="py-5">
             <div className="container px-4 px-lg-5 mt-5">
                 <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
